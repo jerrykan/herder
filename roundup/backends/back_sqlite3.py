862beecee1a5411937b5f3d6640f80fb89130026
John Kristensen <john@jerrykan.com>
2015-01-05 17:52:01 +1100

    Start reworking sqlite3 backend on sqlalchemy with tests

--- roundup/backends/back_sqlite3.py
+++ roundup/backends/back_sqlite3.py
@@ -1,13 +1,30 @@
-import logging
 import os
-import sqlite3
 
-from . import rdbms_common
-from .blobfiles import FileStorage
+from sqlalchemy import Column, Index, MetaData, Table, create_engine, types
+
+from roundup import hyperdb, security
+from roundup.backends import rdbms_common
+from .rdbms_common import FileClass, IssueClass
+from .sqlalchemy_common import SqlAlchemyDatabase
+
+TYPE_MAP = {
+    hyperdb.String: types.String,
+    hyperdb.Date: types.DateTime,
+    hyperdb.Link: types.Integer,
+    hyperdb.Interval: types.Interval,
+    hyperdb.Password: types.String,
+    hyperdb.Boolean: types.Boolean,
+    hyperdb.Number: types.Float,
+}
 
 
-class MissingSchema(sqlite3.OperationalError):
+class Class(rdbms_common.Class):
     pass
+
+
+# TODO: placeholder
+def db_exists(*args, **kwargs):
+    return False
 
 
 def _temporary_db(db_name):
@@ -17,133 +34,131 @@ def _temporary_db(db_name):
     if db_name.lower().startswith(':memory:'):
         return True
 
-    if db_name.lower().startswith('file::memory:'):
-        return True
-
-    try:
-        params = db_name.split('?', 1)[1].split('&')
-        if 'mode=memory' in params:
-            return True
-    except IndexError:
-        pass
-
     return False
 
 
-#class Database(SqliteFileStorage, rdbms_common.Database):
-class Database(rdbms_common.Database, object):
-    arg = '?'
-
-    # TODO: some of this stuff should probably move back into rdbms_common
+class Database(SqlAlchemyDatabase):
     def __init__(self, config, journaltag=None):
-        self.logger = logging.getLogger('roundup.hyperdb')
+        ### TODO: ripped from rdbms_common
+        self.classes = {}
+        self.security = security.Security(self)
+        ###
 
-        self.db_name = config.RDBMS_NAME
-        self.db_path = config.DATABASE
-        self.init_backend_config(config)
+        # TODO: open logger
+        self.journaltag = journaltag
 
-        super(Database, self).__init__(config, journaltag=journaltag)
+        db_path = config.DATABASE
+        db_name = config.RDBMS_NAME
 
-    # TODO: implement in rdbmd_common
-    def init_backend_config(self, config):
-        self.sqlite_timeout = config.RDBMS_SQLITE_TIMEOUT
+        if not _temporary_db(db_name):
+            if not os.path.isdir(db_path):
+                os.makedirs(db_path)
+            
+            self.engine = create_engine('sqlite:///{0}'.format(
+                os.path.join(db_path, db_name)))
+        else:
+            self.engine = create_engine('sqlite:///{0}'.format(db_name))
 
-    # TODO: overriden rdbms_common
-    def load_dbschema(self):
-        """ Load the schema definition that the database currently implements
+        self.conn = self.engine.connect()
+        self.schema = MetaData(bind=self.engine)
+        Table('otks', self.schema,
+            Column('otk_key', types.String, index=True),
+            Column('otk_value', types.String),
+            Column('otk_time', types.Integer),
+        )
+        Table('sessions', self.schema,
+            Column('session_key', types.String, index=True),
+            Column('session_time', types.Integer),
+            Column('session_value', types.String),
+        )
+        Table('__textids', self.schema,
+            Column('_class', types.String),
+            Column('_itemid', types.String),
+            Column('_prop', types.String),
+            Column('_textid', types.Integer, primary_key=True),
+        )
+        Table('__words', self.schema,
+            Column('_word', types.String, index=True),
+            Column('_textid', types.Integer, index=True),
+        )
+        Index('__textids_by_props',
+            self.schema.tables['__textids'].columns._class,
+            self.schema.tables['__textids'].columns._itemid,
+            self.schema.tables['__textids'].columns._prop,
+            unique=True,
+        )
+        # NOTE: we don't store the schema dump in db, it is checked in post_init
+
+    def post_init(self):
+        """ Called once the schema initialisation has finished.
+
+            We should now confirm that the schema defined by our "classes"
+            attribute actually matches the schema in the database.
+        """
+        # TODO: check if the database can be altered (config.RDBMS_ALLOW_ALTER)
+        for classname, spec in self.classes.items():
+            columns = [
+               Column('_actor', TYPE_MAP[hyperdb.Link]),
+               Column('_activity', TYPE_MAP[hyperdb.Date]),
+               Column('_creator', TYPE_MAP[hyperdb.Link]),
+               Column('_creation', TYPE_MAP[hyperdb.Date]),
+            ]
+
+            for name, prop in spec.properties.items():
+                columns.append(
+                    Column('_{0}'.format(name), TYPE_MAP[type(prop)]))
+
+            columns.sort(key=lambda x: x.name)
+            columns += [
+               Column('id', types.Integer, primary_key=True),
+               Column('__retired__', types.Boolean, default=False, index=True),
+            ]
+
+            Table('_{0}'.format(classname), self.schema, *columns)
+    ##
+    ## NOT TESTED BEYOND HERE
+    ##
+
+    # TODO: ripped from rdbms_common
+    #   used by detectors
+    def __getattr__(self, classname):
+        """ A convenient way of calling self.getclass(classname).
+        """
+        if classname in self.classes:
+            return self.classes[classname]
+        raise AttributeError(classname)
+
+    # TODO: taken from rdbms_common
+    def addclass(self, class_):
+        classname = class_.classname
+        if classname in self.classes:
+            raise ValueError(
+                "'{0}' class has already be defined.".format(classname))
+
+        self.classes[classname] = class_
+
+        # TODO: not tested
+        desc = 'User is allowed to {{0}} {0}'.format(classname)
+        for perm in ('Create', 'Edit', 'View', 'Retire'):
+            self.security.addPermission(
+                name=perm, klass=classname,
+                description=desc.format(perm.lower()))
+
+    # TODO: ripped from rdbms_common
+    #   used by instance.open()
+    def getclasses(self):
+        """ Return a list of the names of all existing classes.
+        """
+        return sorted(self.classes)
+
+    # TODO: ripped from rdbms_common
+    #   used by security.addPermissionToRole() in schema.py
+    def getclass(self, classname):
+        """Get the Class object representing a particular class.
+
+        If 'classname' is not a valid class name, a KeyError is raised.
         """
         try:
-            self.cursor.execute('select schema from schema')
-        except sqlite3.OperationalError as err:
-            raise MissingSchema(str(err))
-
-        schema = self.cursor.fetchone()
-        if schema:
-            self.database_schema = eval(schema[0])
-        else:
-            self.database_schema = {}
-
-    def open_connection(self):
-        """ Open a connection to the database, creating it if necessary.
-
-            Must call self.load_dbschema()
-        """
-        #
-        # TODO: temporary DBs may not work because roundup frequently calls
-        #   open/close
-        if not _temporary_db(self.db_name):
-            db_name = os.path.join(self.db_path, self.db_name)
-
-            # ensure the database directory exist
-            if not os.path.isdir(os.path.dirname(db_name)):
-                os.makedirs(os.path.dirname(db_name))
-        else:
-            db_name = self.db_name
-
-        self.logger.info('open database %s', db_name)
-        self.conn = sqlite3.connect(db_name, timeout=self.sqlite_timeout)
-        self.conn.row_factory = sqlite3.Row
-
-        # sqlite3 want us to store Unicode in the db but that's not what's been
-        # done historically and it's definitely not what the other backends do,
-        # so we'll stick with UTF-8
-        # TODO PYTHON3: this will need to be looked at when str defaults to
-        #   unicode
-        self.conn.text_factory = str
-
-        self.cursor = self.conn.cursor()
-
-        try:
-            self.load_dbschema()
-            return
-        except MissingSchema:
-            pass
-
-        self.init_dbschema()
-
-        # TODO SQLALCHEMY: handle database create/migrate stuff
-        # create schema table
-        self.sql('CREATE TABLE schema (schema TEXT)')
-        # create ids table
-        self.sql('CREATE TABLE ids (name TEXT, num INTEGER)')
-        self.sql('CREATE INDEX ids_name_idx ON ids (name)')
-        # create otks table
-        self.sql(
-            'CREATE TABLE otks ' +
-            '(otk_key TEXT, otk_value TEXT, otk_time INTEGER)')
-        self.sql('CREATE INDEX otks_key_idx ON otks (otk_key)')
-        # create sessions table
-        self.sql(
-            'CREATE TABLE sessions ' +
-            '(session_key TEXT, session_time INTEGER, session_value TEXT)')
-        self.sql('CREATE INDEX sessions_key_idx ON sessions (session_key)')
-
-        # full-text indexing store
-        self.sql('CREATE TABLE __textids ' +
-            '(_class varchar, _itemid TEXT, _prop TEXT, ' +
-            '_textid INTEGER PRIMARY KEY)')
-        self.sql(
-            'CREATE UNIQUE INDEX __textids_by_props ' +
-            'ON __textids (_class, _itemid, _prop)')
-
-        self.sql('CREATE TABLE __words (_word TEXT, _textid INTEGER)')
-        self.sql('CREATE INDEX words_word_ids ON __words(_word)')
-        self.sql('CREATE INDEX words_by_id ON __words(_textid)')
-
-        self.sql('INSERT INTO ids (name, num) VALUES (?, ?)', ('__textids', 1))
-
-
-    # hyperdb
-    # roundupdb
-
-
-class Class(rdbms_common.Class):
-    pass
-
-
-class FileClass(rdbms_common.FileClass):
-    pass
-
-
-class IssueClass(rdbms_common.IssueClass):
-    pass
+            return self.classes[classname]
+        except KeyError:
+            raise KeyError('There is no class called "%s"'%classname)
--- tests/test_backend_sqlite3.py
+++ tests/test_backend_sqlite3.py
@@ -1,11 +1,11 @@
-import sqlite3
-from os import path
 from unittest import TestCase
+from os import path
 
-from mock import patch
+from mock import Mock, patch
 
-from roundup.configuration import CoreConfig
-from roundup.backends.back_sqlite3 import Database, MissingSchema, _temporary_db
+from roundup import hyperdb
+from roundup.backends.back_sqlite3 import Database, _temporary_db
+from roundup.backends.back_sqlite3 import Class
 
 
 class TemporaryDbTest(TestCase):
@@ -15,58 +15,103 @@ class TemporaryDbTest(TestCase):
     def test_inmemory_db_name(self):
         self.assertTrue(_temporary_db(':memory:'))
 
-    def test_inmemory_db_file_name(self):
-        self.assertTrue(_temporary_db('file::memory:'))
-
-    def test_inmemory_db_query_param(self):
-        self.assertTrue(_temporary_db(
-            'file:memdb1?cached=shared&mode=memory'))
-
-    def test_not_inmemory_db_query_param(self):
-        self.assertFalse(_temporary_db(
-            'file:memdb1?cached=shared'))
+    def test_filename_db(self):
+        self.assertFalse(_temporary_db('roundup.db'))
 
 
-class OpenConnectionTest(TestCase):
-    @patch('roundup.backends.back_sqlite3.sqlite3.connect')
-    @patch('roundup.backends.back_sqlite3.os.makedirs')
-    def test_temporary_db(self, m_makedirs, m_connect):
-        config = CoreConfig()
-        config.RDBMS_NAME = ':memory:'
+@patch('roundup.backends.back_sqlite3.os.makedirs')
+class InitDatabaseTest(TestCase):
+    def setUp(self):
+        self.config = Mock()
+        self.config.DATABASE = 'db'
+        self.config.RDBMS_NAME = ':memory:'
 
-        with patch.object(Database, 'load_dbschema') as m_load_dbschema:
-            db = Database(config)
+    def test_inmemory_db(self, m_makedirs):
+        db = Database(self.config)
 
         self.assertEqual(m_makedirs.call_count, 0)
-        self.assertEqual(m_connect.call_args, ((':memory:',), {
-            'timeout': config.RDBMS_SQLITE_TIMEOUT,
-        }))
 
-    @patch('roundup.backends.back_sqlite3.sqlite3.connect')
-    @patch('roundup.backends.back_sqlite3.os.makedirs')
-    def test_file_db(self, m_makedirs, m_connect):
-        config = CoreConfig()
-        config.DATABASE = 'zzaaxx/db'
-        config.RDBMS_NAME = 'roundup.db'
+    def test_temporary_db(self, m_makedirs):
+        self.config.RDBMS_NAME = ''
+        db = Database(self.config)
 
-        with patch.object(Database, 'load_dbschema') as m_load_dbschema:
-            db = Database(config)
+        self.assertEqual(m_makedirs.call_count, 0)
 
-        self.assertEqual(m_makedirs.call_count, 1)
-        self.assertEqual(m_connect.call_args, (
-            (path.join('.', 'zzaaxx', 'db', 'roundup.db'),),
-            {'timeout': config.RDBMS_SQLITE_TIMEOUT}
-        ))
+    def test_filename_db(self, m_makedirs):
+        self.config.RDBMS_NAME = 'roundup.db'
 
-    @patch('roundup.backends.back_sqlite3.sqlite3.connect')
-    @patch('roundup.backends.back_sqlite3.os.makedirs')
-    def test_schema_error(self, m_makedirs, m_connect):
-        config = CoreConfig()
-        config.DATABASE = 'zzaaxx/db'
-        config.RDBMS_NAME = 'roundup.db'
+        p_isdir = patch('roundup.backends.back_sqlite3.os.path.isdir',
+                        return_value=False)
+        p_create_engine = patch('roundup.backends.back_sqlite3.create_engine')
 
-        with (patch.object(Database, 'load_dbschema') as m_load_dbschema,
-              patch.object(Database, 'sql') as m_sql):
-#            m_load_dbschema.side_effect = sqlite3.OperationalError
-            m_load_dbschema.side_effect = MissingSchema
-            db = Database(config)
+        with p_isdir, p_create_engine as m_create_engine:
+            db = Database(self.config)
+
+            self.assertEqual(
+                m_create_engine.call_args[0],
+                ('sqlite:///{0}'.format(path.join('db', 'roundup.db')),)
+            )
+            self.assertEqual(m_create_engine.call_args[1], {})
+
+        self.assertEqual(m_makedirs.call_args, (('db',), {}))
+
+
+class PostInitDatabase(TestCase):
+    def setUp(self):
+        config = Mock()
+        config.DATABASE = ''
+        config.RDBMS_NAME = ''
+        self.db = Database(config)
+
+    def test_empty_schema(self):
+        pass
+
+    def test_simple_table(self):
+        person = Class(self.db, 'person',
+            name=hyperdb.String(),
+            age=hyperdb.Number(),
+            birthday=hyperdb.Date()
+        )
+        self.db.post_init()
+
+        self.assertIn('_person', self.db.schema.tables)
+        import pdb; pdb.set_trace()
+        
+#    def test_it(self):
+#        person = Class(self.db, 'person',
+#            name=hyperdb.String(),
+#            age=hyperdb.Number(),
+#            birthday=hyperdb.Date(),
+#            gender=hyperdb.Link('gender'),
+##            jobs=hyperdb.Multilink('job')
+#        )
+#
+#        
+#
+#        gender = Class(self.db, 'gender',
+#            name=hyperdb.String()
+#        )
+#        job = Class(self.db, 'job',
+#            name=hyperdb.String(),
+#        )
+#
+#        self.db.post_init()
+#        import pdb; pdb.set_trace()
+
+
+
+class AddClassDatabaseTest(TestCase):
+    def setUp(self):
+        config = Mock()
+        config.DATABASE = ''
+        config.RDBMS_NAME = ''
+        self.db = Database(config)
+
+    def test_add_class(self):
+        c = Class(self.db, 'test')
+        self.assertEqual(self.db.classes, {'test': c})
+        # TODO check permissions once getclass is implemented
+
+    def test_redefined_class(self):
+        Class(self.db, 'test')
+        self.assertRaises(ValueError, Class, self.db, 'test')
