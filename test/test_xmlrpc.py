idiotic-geek/roundup-mirror-via-github-import

Update tests to work with py.test
py.test searches for any class that looks like a TestCase in the test
directory and tries to run them as tests. Some of the classes that
inherit TestCase are not meant to be run and are only intended to be
"helper classes". Only the tests of the classes that inherit the "helper
classes" should be run. If we convert these "helper classes" to be
"mixins" py.test should not pick them up.

test/test_xmlrpc.py
@@ -16,7 +16,8 @@ from roundup.cgi import TranslationService
 
 import db_test_base
 
-class TestCase(unittest.TestCase):
+
+class XmlrpcTest(object):
 
     backend = None
 
@@ -245,12 +246,29 @@ class TestCase(unittest.TestCase):
         for n, r in enumerate(result):
             self.assertEqual(r, results[n])
 
+
+class anydbmXmlrpcTest(XmlrpcTest, unittest.TestCase):
+    backend = 'anydbm'
+
+
+class mysqlXmlrpcTest(XmlrpcTest, unittest.TestCase):
+    backend = 'mysql'
+
+
+class sqliteXmlrpcTest(XmlrpcTest, unittest.TestCase):
+    backend = 'sqlite'
+
+
+class postgresqlXmlrpcTest(XmlrpcTest, unittest.TestCase):
+    backend = 'postgresql'
+
+
 def test_suite():
     suite = unittest.TestSuite()
-    for l in list_backends():
-        dct = dict(backend = l)
-        subcls = type(TestCase)('TestCase_%s'%l, (TestCase,), dct)
-        suite.addTest(unittest.makeSuite(subcls))
+    suite.addTest(unittest.makeSuite(anydbmXmlrpcTest))
+    suite.addTest(unittest.makeSuite(mysqlXmlrpcTest))
+    suite.addTest(unittest.makeSuite(sqliteXmlrpcTest))
+    suite.addTest(unittest.makeSuite(postgresqlXmlrpcTest))
     return suite
 
 if __name__ == '__main__':

jerrykan/herder

test/test_xmlrpc.py
@@ -18,7 +18,8 @@ import db_test_base
 
 NEEDS_INSTANCE = 1
 
-class TestCase(unittest.TestCase):
+
+class XmlrpcTest(object):
 
     backend = None
 
@@ -247,12 +248,29 @@ class TestCase(unittest.TestCase):
         for n, r in enumerate(result):
             self.assertEqual(r, results[n])
 
+
+class anydbmXmlrpcTest(XmlrpcTest, unittest.TestCase):
+    backend = 'anydbm'
+
+
+class mysqlXmlrpcTest(XmlrpcTest, unittest.TestCase):
+    backend = 'mysql'
+
+
+class sqliteXmlrpcTest(XmlrpcTest, unittest.TestCase):
+    backend = 'sqlite'
+
+
+class postgresqlXmlrpcTest(XmlrpcTest, unittest.TestCase):
+    backend = 'postgresql'
+
+
 def test_suite():
     suite = unittest.TestSuite()
-    for l in list_backends():
-        dct = dict(backend = l)
-        subcls = type(TestCase)('TestCase_%s'%l, (TestCase,), dct)
-        suite.addTest(unittest.makeSuite(subcls))
+    suite.addTest(unittest.makeSuite(anydbmXmlrpcTest))
+    suite.addTest(unittest.makeSuite(mysqlXmlrpcTest))
+    suite.addTest(unittest.makeSuite(sqilteXmlrpcTest))
+    suite.addTest(unittest.makeSuite(postgresqlXmlrpcTest))
     return suite
 
 if __name__ == '__main__':